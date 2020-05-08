98bf5f17ea4e0119f0cc15b52f56daf391d8ed09
Natanael Freire <natan.nascimento.freire@gmail.com>
2020-05-08 13:35:27 -0300

    The problem with mug.js was lack of ambient lighting. Fixed.

--- js/mug.js
+++ js/mug.js
@@ -1,6 +1,5 @@
 import * as THREE from '../node_modules/three/build/three.module.js';
 import { GLTFLoader } from '../node_modules/three/examples/jsm/loaders/GLTFLoader.js';
-import { RoughnessMipmapper } from '../node_modules/three/examples/jsm/utils/RoughnessMipmapper.js';
 
 var camera, scene, renderer;
 var loader;
@@ -11,44 +10,33 @@ animate();
 
 function init() {
 
-	camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.025, 2000);
-	camera.position.set( - 1.8, 0.6, 2.7 );
+	camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 2000);
+	camera.position.set( 0, 0, 10 );
 
 	scene = new THREE.Scene();
 
 	renderer = new THREE.WebGLRenderer( { antialias: true } );
+	//renderer.setClearColor( 0xC5C5C3 );
+	//renderer.setPixelRatio( window.devicePixelRatio );
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	document.body.appendChild( renderer.domElement );
 
-	var roughnessMipmapper = new RoughnessMipmapper( renderer );
+	var ambientLight = new THREE.AmbientLight( 0xcccccc );
+	scene.add( ambientLight );
+
 
 	loader = new GLTFLoader();
 	loader.load('../models/mug/mug.gltf', function ( gltf ) {
 
-		console.log( gltf );
-
-		gltf.scene.traverse( function ( child ) {
-
-			if ( child.isMesh ) {
-
-				roughnessMipmapper.generateMipmaps( child.material );
-
-			}
-
-		} );
-
+		console.log( gltf.scene );
+		/*gltf.scene.scale.set( 2, 2, 2 );
+		gltf.scene.position.x = 0;
+		gltf.scene.position.y = 0;
+		gltf.scene.position.z = 0;*/
 		scene.add( gltf.scene );
 
-		roughnessMipmapper.dispose();
-
-		animate();
-
-	}, undefined, function ( error) {
-
-		console.log(error);
 
 	} );
-
 }
 
 function animate() {
